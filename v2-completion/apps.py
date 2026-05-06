be179827f20ee8c4612976e6740529c6b7348e2b
aimnrt <united.states.erland@gmail.com>
2026-05-06 23:47:20 +0200

    fixed error

--- v2-completion/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class V2CompConfig(AppConfig):
-    name = 'v2_comp'
-    dpy_package = 'v2_comp.ui_ext'
---
+++ v2_completion/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class V2CompletionConfig(AppConfig):
+    name = 'v2_completion'
+    dpy_package = 'v2_completion.ui_ext'
